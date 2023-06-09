a7ba055890598bfb759cdd2a149f510b86ac9d2c
Shinsuke <135405632+shinsuke-okudaira@users.noreply.github.com>
2023-06-09 22:59:56 +0900

    テストの平均点を計算しよう　提出用

---
+++ Score_Chapter04.java
@@ -0,0 +1,37 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int testScoreA = 10;
+		int testScoreB = 20;
+		int testScoreC = 30;
+		int testScoreD = 40;
+		int testScoreE = 50;
+		int testScoreF = 60;
+		int testScoreG = 70;
+		int testScoreH = 80;
+		int testScoreI = 90;
+		int testScoreJ = 100;
+		int countReceivedTest = 10;
+		
+		int averagePoint = (testScoreA +  testScoreB +  testScoreC +  testScoreD +  testScoreE +  testScoreF +  testScoreG +  testScoreH +  testScoreI +  testScoreJ) / countReceivedTest;
+		
+		System.out.println("Aさんのテストの点数は" + testScoreA +"点です。");
+		System.out.println("Bさんのテストの点数は" + testScoreB +"点です。");
+		System.out.println("Cさんのテストの点数は" + testScoreC +"点です。");
+		System.out.println("Dさんのテストの点数は" + testScoreD +"点です。");
+		System.out.println("Eさんのテストの点数は" + testScoreE +"点です。");
+		System.out.println("Fさんのテストの点数は" + testScoreF +"点です。");
+		System.out.println("Gさんのテストの点数は" + testScoreG +"点です。");
+		System.out.println("Hさんのテストの点数は" + testScoreH +"点です。");
+		System.out.println("Iさんのテストの点数は" + testScoreI +"点です。");
+		System.out.println("Jさんのテストの点数は" + testScoreJ +"点です。");
+		
+		System.out.println("テストの平均点は" + averagePoint + "点です。");
+		
+	}
+
+}
